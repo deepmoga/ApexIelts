3e9d3a91bed6ff2e516c1864086db300d36a0cd1
deepmoga <32519872+deepmoga@users.noreply.github.com>
2018-08-30 10:01:28 +0530

    Apex IElts Design

---
+++ ApexIelts/Controllers/DefaultController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApexIelts.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult CompanyOverview()
+        {
+            return View();
+        }
+        public ActionResult FounderMessage()
+        {
+            return View();
+        }
+        public ActionResult WhyChooseUs()
+        {
+            return View();
+        }
+        public ActionResult OurMission()
+        {
+            return View();
+        }
+        public ActionResult OurVision()
+        {
+            return View();
+        }
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult ServiceDetails()
+        {
+            return View();
+        }
+        public ActionResult ShiningStars()
+        {
+            return View();
+        }
+        // GET: Default/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Default/Create
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        // POST: Default/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ApexIelts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApexIelts.MvcApplication" Language="C#" %>
